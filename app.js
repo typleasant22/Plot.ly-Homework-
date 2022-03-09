2bc8955b254878d7443b62509ea5d56a3bc6d9b4
Tyanna Pleasant <90288887+typleasant22@users.noreply.github.com>
2022-03-09 14:01:11 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,96 +1,147 @@
-var ID=0;
+// id="selDataset" in the index.html
+var select_tag = d3.select("#selDataset");
 
-function selectDemographics(Demographics) {
-    return Demographics(Demographics);{
-        return Demographics.id==ID;
-}
-function selectSamples(Samples) {
-    return Samples.id == ID;
+d3.json("samples.json").then((importedData) => {
+  console.log("importedData")
+  console.log(importedData)
 
-}
+  var subject_ids = importedData.names;
+  // var subject_ids = importedData["names"];
+  console.log("Subject_ids")
+  console.log(subject_ids)
 
-//Function to build the bar chart
-function createBarCart(BarSamples){
-    let title= 'Top 10 OTUs found in subject ID';
-    let OTUIDarray= [];
-    for(let i=0; i<10; i++){
-        OTUIDarray.push("OTU"+ BarSamples.otu_ids[i]);
+  subject_ids.forEach((id) => {
+    select_tag
+      .append("option")
+      .property("value", id)
+      .text(id);
+  });
+
+  // index.html is loaded with the dashboard of 940 for the initial page load
+  optionChanged(subject_ids[0]);
+
+});
+
+// The function is triggered by an option change in the Dropdown box of "Test Subject ID No" in index.html
+// <select id="selDataset" onchange="optionChanged(this.value)">
+function optionChanged(selected_id) {
+  console.log("selected_id=", selected_id);
+
+  d3.json("samples.json").then((data) => {
+    //   Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+    // * Use`sample_values` as the values for the bar chart.
+    // * Use`otu_ids` as the labels for the bar chart.
+    // * Use`otu_labels` as the hovertext for the chart.
+
+    var samples = data.samples;
+    var results = samples.filter(sampleObj => sampleObj.id == selected_id);
+
+    console.log("samples: ");
+    console.log(samples);
+
+    var result = results[0];
+
+    console.log("results: ");
+    console.log(results);
+
+    console.log("result: ");
+    console.log(result);
+
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+
+    var y_label = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
+    console.log("y_label: ");
+    console.log(y_label);
+
+    console.log("sample_valuese: ");
+    console.log(sample_values.slice(0, 10).reverse());
+
+    var bar_trace = {
+      y: y_label,
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h",
     };
 
-//Build the bubble chart
-function createBubbleChart(BubbleSamples){
+    var data = [bar_trace];
 
-let MarketArray=[]
-
-for(let i=0; i<Bubblesamples.otu_ids.length; i++){
-    MarketArray.push(Math.sqrt(BubbleSamples.sample_values[i])*5);
-};
-    var bubbletrace = [{
-        x: BubbleSamples.otu_ids,
-        y: BubbleSamples.sample_values,
-        text: text,
-        mode: 'markers',
-        hovertext: Bubblesamples.otu_labels,
-        marker: {
-            colorscale: "Earth",
-            size: MarketArray,
-            color: BubbleSamples.otu_ids
-        }
-    }];
-
-    var layout = {
-        showlegend: false,
-        xAxis: {title: "OTU ID"}
+    var bar_layout = {
+      title: "Top 10 OTUs",
+      margin: { t: 30, l: 150 }
     };
 
-    Plotly.newPlot('bubble', data, bubblelayout);
+    Plotly.newPlot("bar", data, bar_layout);
+
+    //==================================================
+
+    // Create a bubble chart that displays each sample.
+    // * Use`otu_ids` for the x values.
+    // * Use`sample_values` for the y values.
+    // * Use`sample_values` for the marker size.
+    // * Use`otu_ids` for the marker colors.
+    // * Use`otu_labels` for the text values.
+
+    var results = samples.filter(sampleObj => sampleObj.id == selected_id);
+    var result = results[0];
+
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+
+    var bubble_trace = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
+      }
+    };
+
+    var data = [bubble_trace];
+
+    var bubble_layout = {
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: { t: 30 }
+    };
+
+    Plotly.newPlot("bubble", [bubble_trace], bubble_layout);
+  });
+  //====================================================
+
+  // Demorgrphic info
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+
+    console.log("metadata");
+    console.log(metadata);
+
+    var results = metadata.filter(metadataObj => metadataObj.id == selected_id);
+    var result = results[0];
+
+    console.log("results")
+    console.log(results)
+
+    console.log("result")
+    console.log(result)
+
+    var fig = d3.select("#sample-metadata");
+
+    fig.html("");
+
+    Object.entries(results[0]).forEach(([key, value]) => {
+      fig.append("h5").text(`${key}: ${value}`);
+    });
+   });
 }
 
-console.log(option);
-
-d3.json("samples.json").then(function(sampledata) {
-    Names = sampledata.names;
-    Demographics = sampledata.metadata
-    samples= sampledata.samples;
-    ID = option;
-    var FilterDemographics = Demographics.filter(selectDemographics);
-    var FilterSamples = samples.filter(selectSamples);
-    console.log(FilterDemographics);
-    console.log(Filtersamples);
-
-    createDemData(FilterDemographics[0]);
-    createBarCart(Filtersamples[0]);
-    createBubbleChart(filtersamples[0]);
-    createGuageChart(FilterDemographics[0])
-
-    })
- };
-
-//reading the data to display the selected ID
-d3.json("samples.json").then(function(sampledata) {
-    console.log(sampledata);
-
-    Names=sampledata.names;
-    Demographics= sampledata.metadata;
-    Samples= sampledata.samples;
 
 
-    ID=Names[0];
 
-    console.log(names);
-    console.log(Demographics);
-    console.log(Sammples);
-    Builddropdown(names);
-
-    var FilterDemographics = Demographics.filter(selectDemographics);
-    var FilterSamples = samples.filter(selectSamples);
-    console.log(FilterDemographics);
-    console.log(Filtersamples);
-
-    createDemData(FilterDemographics[0]);
-    createBarCart(Filtersamples[0]);
-    createBubbleChart(filtersamples[0]);
-    createGuageChart(FilterDemographics[0]);
-
-    })
-};
